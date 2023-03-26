14709a2d1101aa29c9afe7d774def3d5524b1e16
kevina-code <braveitnow@protonmail.com>
2023-03-26 15:17:12 -0600

    Improvements

--- lwc/multiSelectLookup/multiSelectLookup.js
+++ lwc/multiSelectLookup/multiSelectLookup.js
@@ -12,6 +12,8 @@ import retrieveSearchData from "@salesforce/apex/MultiSelectLookupCtrl.retrieveS
 export default class MultiSelectLookup extends LightningElement {
   @api recordId;
   @api Label;
+  @api context = "multi select lookup";
+  @api recordLimit = "10";
 
   // target configs:
   @api iconName;
@@ -20,13 +22,13 @@ export default class MultiSelectLookup extends LightningElement {
   @api whereClause;
 
   searchWrappers = [];
+  searchResultRecords = [];
   selectedRecords = [];
 
   messageFlag = false;
   isSearchLoading = false;
   @api placeholder = "Search..";
   searchKey;
-  delayTimeout;
 
   // method to retrieve records based on user search
   searchField() {
@@ -41,16 +43,21 @@ export default class MultiSelectLookup extends LightningElement {
       value: this.searchKey,
       selectedRecordIds: selectedRecordIds,
       whereClause: this.whereClause,
-      recordId: this.recordId
+      recordId: this.recordId,
+      recordLimit: this.recordLimit
     })
       .then((result) => {
         this.searchWrappers = result;
+        this.searchResultRecords = this.searchWrappers.map((obj) => obj.record);
+
         this.isSearchLoading = false;
-        const lookupInputContainer = this.template.querySelector(
-          ".lookupInputContainer"
-        );
-        const clsList = lookupInputContainer.classList;
-        clsList.add("slds-is-open");
+        if (this.context === "multi select lookup") {
+          const lookupInputContainer = this.template.querySelector(
+            ".lookupInputContainer"
+          );
+          const clsList = lookupInputContainer.classList;
+          clsList.add("slds-is-open");
+        }
 
         if (this.searchKey.length > 0 && result.length == 0) {
           this.messageFlag = true;
@@ -72,75 +79,100 @@ export default class MultiSelectLookup extends LightningElement {
 
   // update searchKey property on input field change
   handleKeyChange(event) {
-    this.isSearchLoading = true;
-    window.clearTimeout(this.delayTimeout);
-    const searchKey = event.target.value;
-    this.delayTimeout = setTimeout(() => {
-      this.searchKey = searchKey;
-      this.searchField();
-    }, 300);
+    // ignore the following key presses: shift, control, command, etc... but include the delete key and numbers/letters:
+    if ((event.keyCode <= 90 && event.keyCode >= 48) || event.keyCode === 8) {
+      this.isSearchLoading = true;
+      const searchKey = event.target.value;
+      if (searchKey) {
+        this.searchKey = searchKey;
+        setTimeout(() => {
+          this.searchField();
+        }, "300");
+      } else {
+        setTimeout(() => {
+          this.searchWrappers = [];
+          this.searchResultRecords = [];
+          this.selectedRecords = [];
+          this.isSearchLoading = false;
+        }, "300");
+      }
+
+      setTimeout(() => {
+        const searchResultRecords = this.searchResultRecords;
+        const searchedEvent = new CustomEvent("searched", {
+          detail: { searchResultRecords }
+        });
+        this.dispatchEvent(searchedEvent);
+      }, "300");
+    }
   }
 
   // method to toggle lookup result section on UI
   toggleResult(event) {
-    console.log("event.target.value: ", event.target.value);
-    if (!event.target.value) {
-      this.messageFlag = false;
-    }
-    const lookupInputContainer = this.template.querySelector(
-      ".lookupInputContainer"
-    );
-    const clsList = lookupInputContainer.classList;
-    const whichEvent = event.target.getAttribute("data-source");
+    if (this.context === "multi select lookup") {
+      if (!event.target.value) {
+        this.messageFlag = false;
+      }
+      const lookupInputContainer = this.template.querySelector(
+        ".lookupInputContainer"
+      );
+      const clsList = lookupInputContainer.classList;
+      const whichEvent = event.target.getAttribute("data-source");
 
-    switch (whichEvent) {
-      case "searchInputField":
-        clsList.add("slds-is-open");
-        this.searchField();
-        break;
-      case "lookupContainer":
-        clsList.remove("slds-is-open");
-        break;
+      switch (whichEvent) {
+        case "searchInputField":
+          clsList.add("slds-is-open");
+          this.searchField();
+          break;
+        case "lookupContainer":
+          clsList.remove("slds-is-open");
+          break;
+      }
     }
   }
 
   setSelectedRecord(event) {
-    this.messageFlag = false;
-    var recId = event.target.dataset.id;
-    let pluckedRecords = this.searchWrappers.map((obj) => obj.record);
-    let applicableRecords = pluckedRecords.find((data) => data.Id === recId);
-    this.selectedRecords.push(applicableRecords);
-    this.template
-      .querySelector(".lookupInputContainer")
-      .classList.remove("slds-is-open");
-    let selRecords = this.selectedRecords;
-    this.template.querySelectorAll("lightning-input").forEach((each) => {
-      each.value = "";
-    });
+    if (this.context === "multi select lookup") {
+      this.messageFlag = false;
+      var recId = event.target.dataset.id;
+      let applicableRecords = this.searchResultRecords.find(
+        (data) => data.Id === recId
+      );
+      this.selectedRecords.push(applicableRecords);
+      this.template
+        .querySelector(".lookupInputContainer")
+        .classList.remove("slds-is-open");
+      let selRecords = this.selectedRecords;
+      this.template.querySelectorAll("lightning-input").forEach((each) => {
+        each.value = "";
+      });
 
-    const selectedEvent = new CustomEvent("selected", {
-      detail: { selRecords }
-    });
+      const userinput = this.template.querySelector('[data-id="userinput"]');
+      userinput.focus();
+      userinput.click();
 
-    const userinput = this.template.querySelector('[data-id="userinput"]');
-    userinput.focus();
-    userinput.click();
-
-    this.dispatchEvent(selectedEvent);
+      const selectedEvent = new CustomEvent("selected", {
+        detail: { selRecords }
+      });
+      this.dispatchEvent(selectedEvent);
+    }
   }
 
   removeRecord(event) {
-    let selectedRecordIds = [];
-    for (let i = 0; i < this.selectedRecords.length; i++) {
-      if (event.detail.name !== this.selectedRecords[i].Id)
-        selectedRecordIds.push(this.selectedRecords[i]);
-    }
+    if (this.context === "multi select lookup") {
+      let selectedRecordIds = [];
+      for (let i = 0; i < this.selectedRecords.length; i++) {
+        if (event.detail.name !== this.selectedRecords[i].Id)
+          selectedRecordIds.push(this.selectedRecords[i]);
+      }
 
-    this.selectedRecords = [...selectedRecordIds];
-    let selRecords = this.selectedRecords;
-    const selectedEvent = new CustomEvent("selected", {
-      detail: { selRecords }
-    });
-    this.dispatchEvent(selectedEvent);
+      this.selectedRecords = [...selectedRecordIds];
+      let selRecords = this.selectedRecords;
+
+      const selectedEvent = new CustomEvent("selected", {
+        detail: { selRecords }
+      });
+      this.dispatchEvent(selectedEvent);
+    }
   }
 }
